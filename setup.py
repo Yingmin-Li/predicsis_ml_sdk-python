90d521944aa94987065d96fb7969af3786656a63
MKS <michal.szczerbak@predicsis.com>
2015-01-06 10:48:30 +0100

    version 1 info update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='predicsis_ml_sdk',
-    version='0.1.0',
+    version='1.0.0',
     author='Michal K. Szczerbak',
     author_email='michal.szczerbak@predicsis.com',
     packages=['predicsis', 'predicsis.tests'],
